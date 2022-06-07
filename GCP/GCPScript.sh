0e64eb5e4e8f73b09a39032bc55d890e72d5173e
Eyji <eyji.koike@gmail.com>
2022-06-06 21:25:13 -0300

    Alterado shell script para a GCP

--- GCP/GCPScript.sh
+++ GCP/GCPScript.sh
@@ -1,24 +1,57 @@
+#Script .sh pra rodar e automatizar o provisionamento da infraestrutura na nuvem
+#Este codigo provisiona do IoT hub, que funciano como porta de entrada e metodo de autenticacao, 
+#ate o servico Pub/Sub que le as mensagens recebidas e as publica num topico
+
 export PROJECT_ID= #insira o nome do projeto aqui
 export REGION= #insira a regiao aqui
 export TOPIC_ID= #insisra a ID do topico aqui
 export SUBSCRIPTION= #insisra a subscricao aqui
 export REGISTRY= #insisra o nome do registro aqui
 export DEVICE_ID= #insira a identificacao do dispositivo
-export USER_NAME= #insisra o nome de usuario
-export IMAGE_NAME= #insira o nome da imagem
-export SERVICE_NAME= #insira o nome do servico
 
-gcloud services enable cloudiot.googleapis.com pubsub.googleapis.com
+#Aqui inicia o processo
+#Autoriza o Cloud Shell para fazer alteracoes
+gcloud auth login
 
+#O comando abaixo cria um novo projeto. Caso ainda nao tenha criado via console descomente a linha abaixo
+#gcloud projects create $PROJECT_ID
+
+#Aqui temos certeza de que estamos provisionando a infraestrutura no projeto correto
+gcloud config set project $PROJECT_ID
+
+#Ativar as APIs CloudIoT e PubSub
+gcloud services enable cloudiot.googleapis.com pubsub.googleapis.com cloudfunctions.googleapis.com
+
+#Adicionar conta de servico do cloud IoT para poder publicar no Pub/Sub
 gcloud projects add-iam-policy-binding $PROJECT_ID \
     --member=serviceAccount:cloud-iot@system.gserviceaccount.com \
     --role=roles/pubsub.publisher
 
+#Criar um topico no Pub/Sub
 gcloud pubsub topics create $TOPIC_ID
 
+#Criar uma subscricao com o nome definido no topico
 gcloud pubsub subscriptions create --topic $TOPIC_ID $SUBSCRIPTION
 
+#cria o registro na regiao escolhida e ativa mqtt e http
 gcloud iot registries create $REGISTRY \
     --region=$REGION \
     --event-notification-config=topic=temperature-topic \
     --enable-mqtt-config --enable-http-config
+
+#cria uma chave privada 256
+openssl ecparam -genkey -name prime256v1 -noout -out ec_private.pem
+
+#cria uma chave publica 256
+openssl ec -in ec_private.pem -pubout -out ec_public.pem
+
+#realiza o registro do dispositivo com a chave publica
+gcloud iot devices create $DEVICE_ID \
+    --region=$REGION \
+    --registry=$REGISTRY \
+    --public-key="path=./ec_public.pem,type=es256"
+
+#realiza a criacao da funcao que e ativada quando uma mensagem e recebida pelo Pub/Sub
+#Essa funcao precisa ser modificada de acordo com o tipo de DB escolhido e o 
+#Codigo em python precisa ser levado ao cloud shell e tambem adaptado
+gcloud functions deploy iotcore_pubsub_to_influxdb --runtime python37 --trigger-topic $TOPIC_ID --region $REGION
