56de34125f884397ec7c7efde7c85308bc7281c0
Eyji <eyji.koike@gmail.com>
2022-06-08 13:15:35 -0300

    Fixed some conflict issues when generating the board object

--- ArduinoUnoR3/catraca.ino
+++ ArduinoUnoR3/catraca.ino
@@ -1,16 +1,21 @@
 /* Projeto Curto Circuito – Módulo ESP01 - ESP8266 Modo AT */
 
 #include <SoftwareSerial.h>
-SoftwareSerial esp8266(2,3);  /* pino 2 TX , pino 3 RX */
+#include <Arduino.h>
+
+using namespace std;
+SoftwareSerial wiFiBoard(2,3,false);  /* pino 2 TX , pino 3 RX */
+
+
 void setup(){
 Serial.begin(115200);         /* Opções para ajuste 9600, 19200, 57600 e */
-esp8266.begin(115200);        /* 115200 dBs */
+wiFiBoard.begin(115200);        /* 115200 dBs */
 }
 void loop(){
-if(esp8266.available())       /* Confere se a comunicação está acessível */
+if(wiFiBoard.available())       /* Confere se a comunicação está acessível */
 {
-while(esp8266.available()) {  /* Enquanto estiver acessível */
-char c = esp8266.read();      /* Le o caractere. */
+while(wiFiBoard.available()) {  /* Enquanto estiver acessível */
+char c =wiFiBoard.read();      /* Le o caractere. */
 Serial.write(c);              /* Escreve no monitor serial */
 }
 }
@@ -20,6 +25,6 @@ String command="";
 while(Serial.available()) {   /* Le o comando */
 command+=(char)Serial.read();
 }
-esp8266.println(command);    /* Envia o comando para o ESP */
+wiFiBoard.println(command);    /* Envia o comando para o ESP */
 }
 }
