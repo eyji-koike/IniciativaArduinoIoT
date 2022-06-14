6394b69634210795ff0d09c6bcb33e57796de2cd
EyjiCuff <eyji.koike@gmail.com>
2022-06-14 15:15:37 -0300

    restarted the whole code as we weren't able to test the wifi

--- ArduinoUnoR3/catraca.ino
+++ ArduinoUnoR3/catraca.ino
@@ -1,16 +1,46 @@
-/* Projeto Curto Circuito – Módulo ESP01 - ESP8266 Modo AT */
+#include <WiFiEsp.h>
+#include <WiFiEspClient.h>
+#include <WiFiEspUdp.h>
+#include "SoftwareSerial.h"
+#include <PubSubClient.h>
 
-/* Inlcuir as Bibliotecas */
-#include "functions.h"
 
+IPAddress server(10, 0, 0, 2);
+char ssid[] = "DIR-842-4EA9";           // your network SSID (name)
+char pass[] = "18250647";           // your network password
+int status = WL_IDLE_STATUS;   // the Wifi radio's status
 
-/*funcao de setup*/
-void setup(){
-    connectToWiFiNetwork();
-    //mqtt->mqttConnect();
+// Initialize the Ethernet client object
+WiFiEspClient espClient;
+
+SoftwareSerial soft(2,3); // RX, TX
+void setup() {
+  // initialize serial for debugging
+  Serial.begin(9600);
+  // initialize serial for ESP module
+  soft.begin(9600);
+  // initialize ESP module
+  WiFi.init(&soft);
+
+  // check for the presence of the shield
+  if (WiFi.status() == WL_NO_SHIELD) {
+    Serial.println("WiFi shield not present");
+    // don't continue
+    while (true);
+  }
+
+  // attempt to connect to WiFi network
+  while ( status != WL_CONNECTED) {
+    Serial.print("Attempting to connect to WPA SSID: ");
+    Serial.println(ssid);
+    // Connect to WPA/WPA2 network
+    status = WiFi.begin(ssid, pass);
+  }
+
+  // you're connected now, so print out the data
+  Serial.println("You're connected to the network");
 }
 
-/*funcao main*/
 void loop(){
 
 }
